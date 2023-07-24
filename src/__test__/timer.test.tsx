ffc346f6c0cd6e780658879fe80993f6737fc983
jungHyeonS <fbtls25@naver.com>
2023-07-24 23:30:10 +0900

    feat : timer 예제 테스트 코드 작성중

---
+++ src/__test__/timer.test.tsx
@@ -0,0 +1,30 @@
+// 타이머는 초기에 0을 표시합니다.
+// "시작" 버튼을 클릭하면, 타이머는 초 단위로 증가를 시작합니다.
+// "일시정지" 버튼을 클릭하면, 타이머의 진행이 일시정지됩니다.
+// "재설정" 버튼을 클릭하면, 타이머가 0으로 재설정됩니다.
+
+import { screen } from "@testing-library/react";
+
+// 타이머 텍스트가 있는지 테스트
+// 시작 버튼,일시정지,재설정 버튼이 있는지 테스트
+// 시작 버튼이 클릭했을떄, 초가 1초단위로 증가하는지 테스트
+// 일시정지 버튼을 클릭했을떄 타이머가 일시 정지 되는지 테스트
+// 재설정 버튼을 클릭했을떄 타이머가 초기화 되는지 테스트
+
+test("render to timer text element to document", () => {
+  render(<Timer />);
+  const timerText = screen.queryAllByTestId("data-timer-text");
+  expect(timerText).toBeInTheDocument();
+});
+
+test("render to timer contorls to document", () => {
+  render(<Timer />);
+  const startBtn = screen.getByText("시작");
+  expect(startBtn).toBeInTheDocument();
+
+  const stopBtn = screen.getByText("일시정지");
+  expect(stopBtn).toBeInTheDocument();
+
+  const resetBtn = screen.getByText("재설정");
+  expect(stopBtn).toBeInTheDocument();
+});
